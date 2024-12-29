342f298dcb39c33c4d8e8505604365432f4c7da7
Sifon <chenshenghao@163.com>
2024-12-29 11:47:13 +0800

    refactor: 移除代码复制功能并修复版权符号
    - 删除了 libs/util.ts 文件中的所有代码复制相关函数
    - 在 footer 组件中修正了版权符号的表示方式

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-gray-100 mt-auto">
       <div className="max-w-4xl mx-auto px-4 py-8">
         <div className="text-center text-gray-600">
-          <p>© {new Date().getFullYear()} Sifon's博客. All rights reserved.</p>
+          <p>© {new Date().getFullYear()} Sifon&apos;s博客. All rights reserved.</p>
           <div className="mt-2 flex justify-center gap-4">
             <a
               href="https://github.com/halo-sifon/nextjs-blog"
