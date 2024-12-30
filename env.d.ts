60a0ba1129f41a1b80326f6998e5ff4d705b0717
Sifon <chenshenghao@163.com>
2024-12-30 15:17:09 +0800

    fix: use Vercel KV environment variables

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,8 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    UPSTASH_REDIS_REST_URL: string;
-    UPSTASH_REDIS_REST_TOKEN: string;
+    KV_URL: string;
+    KV_REST_API_URL: string;
+    KV_REST_API_TOKEN: string;
+    KV_REST_API_READ_ONLY_TOKEN: string;
   }
 } 
--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
-    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+    KV_URL: process.env.KV_URL,
+    KV_REST_API_URL: process.env.KV_REST_API_URL,
+    KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
+    KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
   },
 }
 
