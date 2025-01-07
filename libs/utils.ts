92bef5b88df01e9bb5d8856471e9bb2e93045123
Sifon <chenshenghao@163.com>
2025-01-07 17:05:16 +0800

    style(utils): 格式化 utils.ts 文件
    - 添加缺失的分号
    - 删除多余的空行
    - 统一代码风格

--- libs/utils.ts
+++ libs/utils.ts
@@ -1,13 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { toast } from 'sonner';
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { toast } from "sonner";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const showToast = {
   success: (message: string) => toast.success(message),
   error: (message: string) => toast.error(message),
   info: (message: string) => toast.info(message),
-}; 
+};
